edb1f82271841f33bf102848094971b537a88ef7
Crong(조희권) <hgjo@gabia.com>
2026-01-13 09:43:58 +0900

    feat: RoomSelector 컴포넌트 구현

---
+++ apps/web/src/components/room-selector.tsx
@@ -0,0 +1,46 @@
+import { Box, TextField, MenuItem, Typography } from '@mui/material';
+
+interface Room {
+  id: string;
+  name: string;
+  capacity: number;
+}
+
+interface RoomSelectorProps {
+  rooms: Room[];
+  selectedRoomId: string | null;
+  onChange: (roomId: string) => void;
+  disabled?: boolean;
+}
+
+export default function RoomSelector({
+  rooms,
+  selectedRoomId,
+  onChange,
+  disabled = false,
+}: RoomSelectorProps) {
+  return (
+    <Box sx={{ mb: 2 }}>
+      <Typography variant="subtitle2" gutterBottom fontWeight={500}>
+        회의실 선택
+      </Typography>
+      <TextField
+        select
+        fullWidth
+        value={selectedRoomId || ''}
+        onChange={(e) => onChange(e.target.value)}
+        disabled={disabled}
+        helperText="확정할 회의실을 선택하세요"
+      >
+        <MenuItem value="">
+          <em>선택 안함</em>
+        </MenuItem>
+        {rooms.map((room) => (
+          <MenuItem key={room.id} value={room.id}>
+            {room.name} (최대 {room.capacity}인)
+          </MenuItem>
+        ))}
+      </TextField>
+    </Box>
+  );
+}
